38487c6988c58305573e3b6ff06430d1507f1645
Aaron Chang <xesuschang@gmail.com>
2019-11-25 21:02:27 +0800

    add method to remove index

--- consistent_hash.go
+++ consistent_hash.go
@@ -1,36 +1,98 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 const (
 	KeyMax = math.MaxUint32
-	KeyMin = uint32(0)
 )
 
 type ConsistentHash interface {
-	Set([]byte)
-	Get([]byte) ([]byte, bool)
+	Add([]byte, []byte)
+	Get([]byte) ([]byte, bool, uint32)
+	Remove(int) error
 }
 
 type consistentHash struct {
 	data map[uint32][]byte
+	keys []uint32
 }
 
-func (c *consistentHash) Set(b []byte) {
-	h := newMyHash(b)
-	var index uint32
-	h.Sum(&index)
+// add key value
+func (c *consistentHash) Add(key []byte, value []byte) {
+	h := hashKey(c, key)
+	c.data[h] = value
+}
 
-	var key uint32
-	for k := range c.data {
-		if index < k {
+func hashKey(c *consistentHash, key []byte) uint32 {
+	h := newMyHash(key)
+	var hashNum uint32
+	h.Sum(&hashNum)
 
+	for i, k := range c.keys {
+		if i == len(c.keys)-1 {
+			break
+		}
+		if k < hashNum && c.keys[i+1] > hashNum {
+			return c.keys[i+1]
 		}
 	}
+	return c.keys[0]
 }
 
-func (c *consistentHash) Get([]byte) ([]byte, bool) {
-	panic("implement me")
+// get value of key
+func (c *consistentHash) Get(key []byte) ([]byte, bool, uint32) {
+	k := hashKey(c, key)
+	if len(c.data[k]) == 0 {
+		return []byte{}, false, 0
+	}
+	return c.data[k], true, k
+}
+
+func (c *consistentHash) Remove(index int) error {
+	storedKey := c.keys[index]
+	var storedValue []byte
+
+	if len(c.data[storedKey]) != 0 {
+		storedValue = c.data[storedKey]
+	}
+
+	removeByIndex(index, c)
+
+	err := redistribute(c, storedKey, storedValue)
+	return err
+}
+
+func removeByIndex(index int, c *consistentHash) {
+	// remove from key
+	k := c.keys[index]
+	if index == 0 {
+		c.keys = c.keys[1:]
+	} else {
+		c.keys = append(c.keys[0:index], c.keys[index+1:]...)
+	}
+
+	// remove from map
+	delete(c.data, k)
+}
+
+func redistribute(c *consistentHash, key uint32, value []byte) error {
+	target := c.keys[0]
+	for i, k := range c.keys {
+		if k < key && c.keys[i+1] > key {
+			target = c.keys[i+1]
+			break
+		}
+	}
+
+	if len(c.data[target]) != 0 {
+		return fmt.Errorf("data already exist")
+	} else {
+		c.data[target] = value
+	}
+	return nil
 }
 
 func newConsistentHash(count int) ConsistentHash {
@@ -38,14 +100,25 @@ func newConsistentHash(count int) ConsistentHash {
 
 	if count == 0 || count == 1 {
 		m[0] = make([]byte, 0)
-		return &consistentHash{data: m}
+		return &consistentHash{
+			data: m,
+			keys: []uint32{0},
+		}
 	}
 
-	interval := KeyMax / (count - 1)
+	interval := KeyMax / count
+	keys := make([]uint32, count)
+	keys[0] = uint32(interval / 2)
+	m[keys[0]] = make([]byte, 0)
 
-	for i := 1; i < count; i += interval {
-		m[uint32(1)] = make([]byte, 0)
+	for i := 1; i < count; i++ {
+		key := uint32(interval * (2*i + 1) / 2)
+		m[key] = make([]byte, 0)
+		keys[i] = key
 	}
 
-	return &consistentHash{data: m}
+	return &consistentHash{
+		data: m,
+		keys: keys,
+	}
 }
--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	h := newMyHash("cat")
-	var out uint32
-	h.Sum(&out)
-	fmt.Printf("%d\n", out)
+	h := newConsistentHash(4)
+	key := []byte("today")
+	value := []byte("hello")
+	h.Add(key, value)
+	fmt.Printf("add key: %s, value: %s\n", string(key), string(value))
+	out, exist, hashKey := h.Get(key)
+	fmt.Printf("value: %s, exist: %t, hash key: %d\n", string(out), exist, hashKey)
+	err := h.Remove(0)
+	if nil != err {
+		fmt.Printf("remove index 1, data already exist")
+		return
+	}
+	out, exist, hashKey = h.Get(key)
+	fmt.Printf("remove 1 value: %s, exist: %t, hash key: %d\n", string(out), exist, hashKey)
 }
